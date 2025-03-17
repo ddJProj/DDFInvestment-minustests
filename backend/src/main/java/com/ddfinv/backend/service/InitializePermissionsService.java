b931c1514b9006b9c953974a5dbd78791fda1545
Edward J. <edd.f.johnson.work@gmail.com>
2025-03-17 11:22:31 -0400

    add additional permissions to the init list. used in controller/backend services for roles

--- backend/src/main/java/com/ddfinv/backend/service/InitializePermissionsService.java
+++ backend/src/main/java/com/ddfinv/backend/service/InitializePermissionsService.java
@@ -23,11 +23,20 @@ public class InitializePermissionsService {
     public void initializePermissions(){
 
         List<Permission> basePermissions = Arrays.asList(
+
+            new Permission("CREATE_EMPLOYEE", "Create Employee account types."),
+            new Permission("VIEW_EMPLOYEE", "View an Employee account's details."),
+            new Permission("VIEW_EMPLOYEES", "View the details of all Employee accounts"),
+            new Permission("EDIT_EMPLOYEE", "Edit the details of an Employee account."),
             new Permission("VIEW_ACCOUNT", "View the details of an account."),
             new Permission("EDIT_ACCOUNT", "Edit the details of an account."),
-            new Permission("VIEW_CLIENT", "View the details of a specific client."),
-            new Permission("EDIT_CLIENT", "Edit the details of a specific client."),
+            new Permission("CREATE_CLIENT", "Create Client account types."),
+            new Permission("VIEW_CLIENT", "View the details of a specific Client."),
+            new Permission("VIEW_CLIENTS", "View the details of all Client accounts."),
+            new Permission("EDIT_CLIENT", "Edit the details of a specific Client."),
+            new Permission("ASSIGN_CLIENT", "Assign a client to an Employee."),
             new Permission("VIEW_INVESTMENT", "View the details of a speecific investment."),
+            new Permission("EDIT_INVESTMENT", "Edit the details of a speecific investment."),
             new Permission("NEW_INVESTMENT", "Define the details of a new investment."),
             new Permission("VIEW_PUBLIC_INFO", "View publically accessible information.")
         );
