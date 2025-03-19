8d4cd4e45c13c079a43fab5413acc88cd167240a
Edward J. <edd.f.johnson.work@gmail.com>
2025-03-19 16:24:06 -0400

    updated the permission initializer to add / save all enum permissions to the repository

--- backend/src/main/java/com/ddfinv/backend/service/InitializePermissionsService.java
+++ backend/src/main/java/com/ddfinv/backend/service/InitializePermissionsService.java
@@ -1,6 +1,7 @@
 package com.ddfinv.backend.service;
 
 import com.ddfinv.core.domain.Permission;
+import com.ddfinv.core.domain.enums.Permissions;
 import com.ddfinv.core.repository.PermissionRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Service;
 import jakarta.annotation.PostConstruct;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 
 @Service
 public class InitializePermissionsService {
@@ -22,29 +24,12 @@ public class InitializePermissionsService {
 
     @PostConstruct
     public void initializePermissions(){
+        Set<Permissions> allPermissionsTypes = Permissions.getAllPermissions();
 
-        List<Permission> basePermissions = Arrays.asList(
-
-            new Permission("CREATE_EMPLOYEE", "Create Employee account types."),
-            new Permission("VIEW_EMPLOYEE", "View an Employee account's details."),
-            new Permission("VIEW_EMPLOYEES", "View the details of all Employee accounts"),
-            new Permission("EDIT_EMPLOYEE", "Edit the details of an Employee account."),
-            new Permission("VIEW_ACCOUNT", "View the details of an account."),
-            new Permission("EDIT_ACCOUNT", "Edit the details of an account."),
-            new Permission("CREATE_CLIENT", "Create Client account types."),
-            new Permission("VIEW_CLIENT", "View the details of a specific Client."),
-            new Permission("VIEW_CLIENTS", "View the details of all Client accounts."),
-            new Permission("EDIT_CLIENT", "Edit the details of a specific Client."),
-            new Permission("ASSIGN_CLIENT", "Assign a client to an Employee."),
-            new Permission("VIEW_INVESTMENT", "View the details of a speecific investment."),
-            new Permission("EDIT_INVESTMENT", "Edit the details of a speecific investment."),
-            new Permission("NEW_INVESTMENT", "Define the details of a new investment."),
-            new Permission("VIEW_PUBLIC_INFO", "View publically accessible information.")
-        );
-
-        for (Permission permission : basePermissions){
-            if (!permissionRepository.existsByName(permission.getName())){
-                permissionRepository.save(permission);
+        for (Permissions permissionType : allPermissionsTypes){
+            if (!permissionRepository.existsByPermissionType(permissionType)){
+                Permission PermissionEntity = new Permission(permissionType, permissionType.getDescription());
+                permissionRepository.save(PermissionEntity);
             }
         }
     }
