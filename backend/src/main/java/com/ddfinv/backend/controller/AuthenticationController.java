d4dbeaa7c4671ba107ad729c3f36bec2b7d59919
Edward J. <edd.f.johnson.work@gmail.com>
2025-03-19 10:41:27 -0400

    define the authController and endpoints for registration / auth

---
+++ backend/src/main/java/com/ddfinv/backend/controller/AuthenticationController.java
@@ -0,0 +1,50 @@
+package com.ddfinv.backend.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ddfinv.backend.dto.auth.AuthenticationRequest;
+import com.ddfinv.backend.dto.auth.AuthenticationResponse;
+import com.ddfinv.backend.dto.auth.RegisterAuthRequest;
+import com.ddfinv.backend.exception.ApplicationException;
+import com.ddfinv.backend.exception.validation.EmailValidationException;
+import com.ddfinv.backend.service.auth.AuthenticationService;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthenticationController {
+    private final AuthenticationService authenticationService;
+
+    public AuthenticationController(AuthenticationService authenticationService){
+        this.authenticationService = authenticationService;
+    }
+
+    /**
+     * 
+     * @param request
+     * @return
+     * @throws EmailValidationException 
+     */
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterAuthRequest request) throws ApplicationException {
+        return ResponseEntity.ok(authenticationService.register(request));
+        
+    }
+
+    /**
+     * 
+     * @param request
+     * @return
+     */
+    @PostMapping("authenticate")
+    public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request) {
+        return ResponseEntity.ok(authenticationService.authenticate(request));
+        
+    }
+    
+}
