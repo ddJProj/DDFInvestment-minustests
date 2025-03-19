3686d05ccf52ca8244f2b3d5b8fa578ddec6056f
Edward J. <edd.f.johnson.work@gmail.com>
2025-03-19 16:22:04 -0400

    updated the permission evaluator to rely on new enum based permissions system instead of string permission names.

--- core/src/main/java/com/ddfinv/core/service/EntityPermissionEvaluator.java
+++ core/src/main/java/com/ddfinv/core/service/EntityPermissionEvaluator.java
@@ -1,12 +1,14 @@
 package com.ddfinv.core.service;
 
+
 import com.ddfinv.core.domain.UserAccount;
+import com.ddfinv.core.domain.enums.Permissions;
 import com.ddfinv.core.domain.enums.Role;
 
 public class EntityPermissionEvaluator implements PermissionEvaluator {
 
     @Override
-    public boolean hasPermission(UserAccount userAccount, String permissionName, Object resourceObject) {
+    public boolean hasPermission(UserAccount userAccount, Permissions permissionType, Object resourceObject) {
         if(userAccount.getRole() == Role.admin){
             return true; // admin has all permissions
         }
@@ -17,7 +19,7 @@ public class EntityPermissionEvaluator implements PermissionEvaluator {
         
         // basic permission check to validate userAccount passed in as arg
         return userAccount.getPermissions().stream() // creates stream from permissions for this account
-        .anyMatch(p->p.getName().equals(permissionName)); // check stream for any permission matching permissionName
+        .anyMatch(p->p.getPermissionType() == permissionType); // check stream for any permission matching permissionName
     }
 
     // VERIFY that the USERACCOUNT attempting an action has valid access to required permission
