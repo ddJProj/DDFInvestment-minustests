3269c3496f81a1c070363469a6568f3b8647af1b
Edward J. <edd.f.johnson.work@gmail.com>
2025-03-20 19:32:50 -0400

    relocated account related services to accounts package

---
+++ backend/src/main/java/com/ddfinv/backend/service/accounts/AdminService.java
@@ -0,0 +1,9 @@
+package com.ddfinv.backend.service.accounts;
+
+public class AdminService {
+
+
+// FIXME: ADD METHODS FOR CHANGING ACCOUNT ROLES
+    //TODO: finish adding admin service related logic
+
+}
