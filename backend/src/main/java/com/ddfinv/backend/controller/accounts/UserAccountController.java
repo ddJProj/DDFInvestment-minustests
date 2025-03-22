bc6069bb0e9d4a310b7dc8668a5184ecccb11318
Edward J. <edd.f.johnson.work@gmail.com>
2025-03-22 10:35:27 -0400

    added getusersbyrole, updateuserpassword , and updateuserrole method definitions

--- backend/src/main/java/com/ddfinv/backend/controller/accounts/UserAccountController.java
+++ backend/src/main/java/com/ddfinv/backend/controller/accounts/UserAccountController.java
@@ -3,6 +3,7 @@ package com.ddfinv.backend.controller.accounts;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -10,16 +11,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ddfinv.backend.dto.accounts.UserAccountDTO;
 import com.ddfinv.backend.dto.actions.UpdatePasswordDTO;
 import com.ddfinv.backend.exception.ResourceNotFoundException;
+import com.ddfinv.backend.exception.database.IllegalOperationException;
 import com.ddfinv.backend.exception.security.InvalidPasswordException;
 import com.ddfinv.backend.service.PermissionHandlerService;
 import com.ddfinv.backend.service.accounts.UserAccountService;
 import com.ddfinv.core.domain.UserAccount;
 import com.ddfinv.core.domain.enums.Permissions;
+import com.ddfinv.core.domain.enums.Role;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -56,6 +61,21 @@ public class UserAccountController {
 
     /**
      * 
+     * @param role
+     * @return
+     */
+    @GetMapping("/by-role/{role}")
+    public ResponseEntity<?> getUsersByRole(@PathVariable Role role) {
+        if (permissionHandlerService.getThisUser().getRole() != Role.admin){
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
+
+        List<UserAccountDTO> userAccounts = userAccountService.getAllUserAccounts(role);
+        return ResponseEntity.ok(userAccounts);
+    }
+
+    /**
+     * 
      * @param id
      * @return
      * @throws ResourceNotFoundException
@@ -71,6 +91,9 @@ public class UserAccountController {
         return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
 
     }
+
+
+
 
     /**
      * 
@@ -112,9 +135,11 @@ public class UserAccountController {
      *
      * @param id
      * @return
+     * @throws ResourceNotFoundException 
+     * @throws IllegalOperationException 
      */
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> deleteUserAccount(@PathVariable Long id){
+    public ResponseEntity<?> deleteUserAccount(@PathVariable Long id) throws IllegalOperationException, ResourceNotFoundException{
         if (!permissionHandlerService.currentUserHasPermission(Permissions.DELETE_USER)){
             return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
         }
@@ -126,6 +151,7 @@ public class UserAccountController {
 
 
     /**
+     * TODO: Add update-my-password vs update-other-password
      *
      * @param ChangePasswordDTO
      * @param entity
@@ -135,17 +161,45 @@ public class UserAccountController {
     @PostMapping("/{id}/update-password")
     public ResponseEntity<?> updateUserAccountPassword(@PathVariable Long id, @RequestBody UpdatePasswordDTO updatePasswordDTO) throws ResourceNotFoundException {
 
-        boolean isOwnAccount = permissionHandlerService.getThisUser().getId().equals(id);
+        UserAccount thisUserAccount = permissionHandlerService.getThisUser();
+        boolean isOwnAccount = thisUserAccount.getId().equals(id); // check to see if changing own password or other account's
 
-        if (!isOwnAccount && !permissionHandlerService.currentUserHasPermission(Permissions.EDIT_USER)){
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
-        }
+        if(isOwnAccount){
+            if (!permissionHandlerService.currentUserHasPermission(Permissions.UPDATE_MY_PASSWORD)){
+                return ResponseEntity.status(HttpStatus.FORBIDDEN).body("You don't have permission to change that password.");
+            }else {
+                if (!permissionHandlerService.currentUserHasPermission(Permissions.UPDATE_OTHER_PASSWORD)){
+                    return ResponseEntity.status(HttpStatus.FORBIDDEN).body("You don't have permission to change that password.");
+                }
+            }
+
+
         try{
-            userAccountService.updateUserAccountPassword(id, updatePasswordDTO);
-            return ResponseEntity.ok().build();
+            // if the user is an admin / is attempting to change the password of a different account
+            if(!isOwnAccount && permissionHandlerService.currentUserHasPermission(Permissions.UPDATE_OTHER_PASSWORD)){
+                userAccountService.resetUserAccountPassword(id, updatePasswordDTO.getNewPassword());
+            }else{
+                // the user is attempting to change the password of their account
+                userAccountService.updateUserAccountPassword(id, updatePasswordDTO);
+            }
+            return ResponseEntity.ok().body("You successfully updated the password for this account.");
         }catch(InvalidPasswordException e){
             return ResponseEntity.badRequest().body(e.getMessage());
         }
+    }
+            return null;
+
+    }
+
+    
+    @PutMapping("/{id}/role")
+    public ResponseEntity<?> changeUserRole(@PathVariable Long id, @RequestParam Role updatedRole) throws IllegalOperationException, ResourceNotFoundException {
+        if (permissionHandlerService.getThisUser().getRole() != Role.admin){
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
+
+        UserAccountDTO updatedUserAccount  = userAccountService.changeUserRole(id, updatedRole);
+        return ResponseEntity.ok(updatedUserAccount);
     }
 
 
