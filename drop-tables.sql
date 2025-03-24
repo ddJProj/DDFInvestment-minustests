31a705f8302fde8cd07e5c30649b6cbd6912f367
Edward J. <edd.f.johnson.work@gmail.com>
2025-03-24 11:23:55 -0400

    added drop tables script for easy terminal reset of database (testing)

---
+++ drop-tables.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS guest_upgrade_request;
+DROP TABLE IF EXISTS client;
+DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS useraccount_permissions;
+DROP TABLE IF EXISTS useraccount;
+DROP TABLE IF EXISTS permission;
+DROP TABLE IF EXISTS client_seq;
+DROP TABLE IF EXISTS employee_seq;
+DROP TABLE IF EXISTS guest_upgrade_request_seq;
+DROP TABLE IF EXISTS permission_seq;
+DROP TABLE IF EXISTS useraccount_seq;
