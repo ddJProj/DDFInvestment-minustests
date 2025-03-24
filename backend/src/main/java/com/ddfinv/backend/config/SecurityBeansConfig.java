12c1d17949c56f59b5db9e0fb043186c915da038
Edward J. <edd.f.johnson.work@gmail.com>
2025-03-24 12:25:34 -0400

    successful backend start - updated beansconfig file to add cors handling (front end wasnt generating auth / login)

--- backend/src/main/java/com/ddfinv/backend/config/SecurityBeansConfig.java
+++ backend/src/main/java/com/ddfinv/backend/config/SecurityBeansConfig.java
@@ -1,5 +1,7 @@
 package com.ddfinv.backend.config;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -9,12 +11,27 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+
+/*
+ * https://docs.spring.io/spring-security/reference/servlet/integrations/cors.html
+ * https://www.danvega.dev/blog/spring-security-cors
+ * https://medium.tuanh.net/cors-with-spring-security-6-6b765f23ca5f
+ * https://spring.io/blog/2015/06/08/cors-support-in-spring-framework
+ * 
+ */
+
+
 
 import com.ddfinv.backend.service.UserDetailService;
 import com.ddfinv.core.service.EntityPermissionEvaluator;
@@ -24,6 +41,7 @@ import com.ddfinv.core.service.PermissionEvaluator;
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityBeansConfig {
+
 
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
     private final UserDetailsService userDetailsService;
@@ -41,13 +59,15 @@ public class SecurityBeansConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-        http.csrf(csrf -> csrf.disable())
+        return http
+        .csrf(AbstractHttpConfigurer::disable)
+        .cors(cors -> cors.configurationSource(corsConfigurationSource()))
         .authorizeHttpRequests(auth -> auth.requestMatchers("/api/auth/**")
         .permitAll().anyRequest().authenticated())
         .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .authenticationProvider(authenticationProvider())
-        .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
-        return http.build();
+        .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+        .build();
     }
 
 
@@ -65,6 +85,18 @@ public class SecurityBeansConfig {
 
     }
 
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder(){
