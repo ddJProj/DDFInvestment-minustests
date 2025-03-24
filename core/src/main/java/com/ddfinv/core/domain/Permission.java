a28ad15e9ffeb63ae560448706e8a90437ec8937
Edward J. <edd.f.johnson.work@gmail.com>
2025-03-24 11:29:18 -0400

    added missing entity and table name annotations

--- core/src/main/java/com/ddfinv/core/domain/Permission.java
+++ core/src/main/java/com/ddfinv/core/domain/Permission.java
@@ -9,13 +9,17 @@ import org.hibernate.annotations.ManyToAny;
 import com.ddfinv.core.domain.enums.Permissions;
 import jakarta.annotation.Generated;
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "permission")
 public class Permission {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
