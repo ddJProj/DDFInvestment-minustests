db0c5b039b0919bcfe39e26b42b7f7617adbce02
Edward J. <edd.f.johnson.work@gmail.com>
2025-03-26 13:54:55 -0400

    updated placeholder base dashboard to handle role specific nav

--- frontend/src/components/pages/dashboard/Dashboard.tsx
+++ frontend/src/components/pages/dashboard/Dashboard.tsx
@@ -1,14 +1,29 @@
-// src/components/pages/dashboard/index.tsx
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { usePermissions } from "../../../hooks/usePermissions";
 import { UserRole } from "../../../types/auth.types";
+import { ROUTES } from "../../../constants/router.constants";
 
 const Dashboard: React.FC = () => {
   const { hasRole, isLoading, error } = usePermissions();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!isLoading && !error) {
+      if (hasRole(UserRole.Admin)) {
+        navigate(ROUTES.ADMIN);
+      } else if (hasRole(UserRole.Employee)) {
+        navigate(ROUTES.EMPLOYEE);
+      } else if (hasRole(UserRole.Client)) {
+        navigate(ROUTES.CLIENT);
+      }
+    }
+  }, [hasRole, isLoading, error, navigate]);
 
   if (isLoading) return <p>Loading...</p>;
   if (error) return <p>{error}</p>;
 
+  // will briefly show before redirection happens
   if (hasRole(UserRole.Admin)) {
     return <p>Welcome Admin! You have full access to manage the system.</p>;
   }
@@ -21,8 +36,7 @@ const Dashboard: React.FC = () => {
     return <p>Welcome Client! Access your account and manage your services here.</p>;
   }
 
-  return <p>Unauthorized or role not recognized</p>;
+  return <p>Unauthorized, Guest, or role not recognized</p>;
 };
 
 export default Dashboard;
-
