44b0c60a58f113b1368de24cf25294bd9d6abe95
Edward J. <edd.f.johnson.work@gmail.com>
2025-03-26 11:43:33 -0400

    added other userAccount details to be returned with the token for use in the frontend

--- backend/src/main/java/com/ddfinv/backend/dto/auth/AuthenticationResponse.java
+++ backend/src/main/java/com/ddfinv/backend/dto/auth/AuthenticationResponse.java
@@ -1,5 +1,9 @@
 package com.ddfinv.backend.dto.auth;
 
+import java.util.Set;
+
+import com.ddfinv.core.domain.enums.Permissions;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,4 +16,11 @@ import lombok.NoArgsConstructor;
 public class AuthenticationResponse {
     private String token;
     private String role; // return the role of a user
+    private Long id;
+    private String email;
+    private String firstName;
+    private String lastName;
+    // Set of Permissions associated with this UserAccount
+    private Set<String> permissions;
+
 }
