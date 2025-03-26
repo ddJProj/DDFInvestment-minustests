71a785cb371c4b9eee6c2338c644c200870893be
Edward J. <edd.f.johnson.work@gmail.com>
2025-03-26 11:45:51 -0400

    removed old duplicated routing logic from original app

--- frontend/src/constants/routes.constants.ts
+++
@@ -1,9 +0,0 @@
-// src/constants/routes.constants.ts
-const ROUTES = {
-  ADMIN_DASHBOARD: "/admin",
-  EMPLOYEE_DASHBOARD: "/employee",
-  CLIENT_DASHBOARD: "/client",
-  UNAUTHORIZED: "/unauthorized",
-};
-
-export default ROUTES;
