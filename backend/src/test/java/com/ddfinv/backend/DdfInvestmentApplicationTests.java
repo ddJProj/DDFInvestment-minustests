bc669b1b7a403c80219cbea4bb2fbdab6a87fc2b
Edward J <edd.f.johnson.work@gmail.com>
2025-04-08 11:38:15 -0400

    main app tests class with always pass test added

--- backend/src/test/java/com/ddfinv/backend/DdfInvestmentApplicationTests.java
+++ backend/src/test/java/com/ddfinv/backend/DdfInvestmentApplicationTests.java
@@ -1,17 +1,51 @@
 package com.ddfinv.backend;
 
-// import org.springframework.boot.SpringApplication;
-// import org.springframework.boot.test.context.SpringBootTest;
-// import org.springframework.context.annotation.Import;
+import static org.junit.jupiter.api.Assertions.*;
 
-// import com.ddfinv.backend.DdfInvestmentApplication;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
-// @SpringBootTest
-// @Import(TestcontainersConfiguration.class)
-// class DdfInvestmentApplicationTests {
-//     public static void main(String[] args) {
-//         SpringApplication.from(DdfInvestmentApplication::main)
-//                 .with(TestcontainersConfiguration.class)
-//                 .run(args);
-//     }
-// }
+import com.ddfinv.backend.DdfInvestmentApplication;
+
+//@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+@Disabled
+class DdfInvestmentApplicationTests {
+
+    // public static void main(String[] args) {
+
+    // }
+    @Test
+    void contextLoading(){
+        // test working?
+        assertTrue(true, "Test always passes, is system configured correctly?");
+    }
+    // public static void main(String[] args) {
+    //     SpringApplication.from(DdfInvestmentApplication::main)
+    //             .with(TestcontainersConfiguration.class)
+    //             .run(args);
+    // }
+} 
+    // @BeforeEach
+    // void setUp() {
+    // }
+
+    // @AfterEach
+    // void tearDown() {
+    // }
+
+    // @Test
+    // void main() {
+    // }
+
+    // @Test
+    // void restTemplate() {
+    // }
